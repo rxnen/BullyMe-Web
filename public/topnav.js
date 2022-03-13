b3594f4184eb4d4af3775d4775b7f73983186793
Bob Brown <bob-brown@Ronens-MBP.hsd1.ca.comcast.net>
2022-03-12 20:49:10 -0800

    changes

---
+++ public/topnav.js
@@ -0,0 +1,6 @@
+function openMobileNav() {
+    document.getElementById("overlayNav").style.width = "100%";
+}
+function closeMobileNav() {
+    document.getElementById("overlayNav").style.width = "0%";
+}
